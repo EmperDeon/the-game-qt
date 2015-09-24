f8834c68fa374e8557c6fb53d974588590eb1cc5
ilz2010 <ilz2010@yandex.ru>
2015-09-24 14:28:51 +0500

    Added overwriting to GVars (list forming in ModLoader)

---
+++ ModLoader/MParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ilz20 on 24.09.2015.
+//
+
+#include "MParser.h"
---
+++ ModLoader/MParser.h
@@ -0,0 +1,14 @@
+//
+// Created by ilz20 on 24.09.2015.
+//
+
+#ifndef GLOBALQT_MPARSER_H
+#define GLOBALQT_MPARSER_H
+
+
+class MParser {
+
+};
+
+
+#endif //GLOBALQT_MPARSER_H
